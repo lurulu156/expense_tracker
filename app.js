d9dd4bf378a7a4ebdc88a78be8f15c6695c34c7a
lurulu156 <kzzlin9@gmail.com>
2022-12-02 15:25:31 +0800

    feat: add create new item feature

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const PORT = 3000
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Category = require('./models/category')
+const Expense = require('./models/expense')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -16,21 +19,34 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
 //setup handlebars
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
+// view all items
 app.get('/', (req, res) => {
   res.render('index')
 })
+//create new item
 app.get('/expense/new', (req, res) => {
   res.render('new')
 })
+app.post('/expense', (req, res) => {
+  const {name, date, category, price} = req.body
+  Category.findOne({name: category})
+  .then(category => Expense.create({name, date, category: category._id, price}))
+  .then(() => res.redirect('/'))
+  .catch(err => console.log(err))
+})
+//edit new item
 app.get('/expense/edit', (req, res) => {
   res.render('edit')
 })
+//delete item
 
 
 app.listen(PORT, () => {
